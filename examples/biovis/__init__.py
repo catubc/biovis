47c5a1ee202da1fda1706bc694701e6bfba8a67d
cat <cat@cat.lab>
2016-10-10 09:48:29 -0700

    removed unwanted files

--- examples/biovis/__init__.py
+++
@@ -1,9 +0,0 @@
-#Visualization toolbox 
-#Created for Allen Institute - Modelling, Analysis and Theory biophysically detailed network models
-#Authors: Catalin Mitelut, Sergey Gratiy
-#MIT License
-
-""" initiatialization file; leave empty
-"""
-
-
